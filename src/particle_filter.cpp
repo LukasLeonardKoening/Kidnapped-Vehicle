0b752ee35b8fb8e1397dc90aa3c7329909a9101a
LukasLeonardKoening <llkoening@posteo.de>
2020-01-13 18:32:22 +0100

    Changed number of particles

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -31,7 +31,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
      TODO: Add random Gaussian noise to each particle.
      NOTE: Consult particle_filter.h for more information about this method (and others in this file).
     */
-    num_particles = 1000;  // TODO: Set the number of particles
+    num_particles = 100;  // TODO: Set the number of particles
     
     // initialization of normal distributions
     normal_distribution<double> dist_x(x, std[0]);
